d99059e3e769d11b7b07d88040fdad213b8f47d5
Bradley Kang <bkang@lcs.on.ca>
2022-01-25 09:02:26 -0500

    Added a function and the ability to delete reminders by swiping left on them

--- Shared/ViewModels/TaskStore.swift
+++ Shared/ViewModels/TaskStore.swift
@@ -8,10 +8,21 @@
 import Foundation
 
 class TaskStore: ObservableObject {
+    
+    // MARK: Stored Properties
     @Published var tasks: [Task]
     
+    // MARK: Initializer
     init(tasks: [Task] = []) {
         self.tasks = tasks
+    }
+    
+    // MARK: Functions
+    func deleteItem(at offsets: IndexSet) {
+        // The "offsets" contain a set of items selected for deletion
+        // The set is then passed to the built-in remove method
+        // This is on the "tasks" array which handles removal from the array
+        tasks.remove(atOffsets: offsets)
     }
 }
 
